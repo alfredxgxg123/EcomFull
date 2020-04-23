73385fe900516ece020d6885623c0ef1f9e8e9c2
Alfred <alfred@kyles-mbp.gateway.sonic.net>
2020-04-23 01:22:10 -0700

    init setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button type="button" onClick={() => setCount(count + 1)}>Count Up To The Moon</button>
+    </div>
+  );
+}
+
+export default App;
