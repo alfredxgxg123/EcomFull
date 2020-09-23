7c451e43c838bdaf164db102e89568cd21696196
Alfredxgxg123 <alfred_guan@hotmail.com>
2020-09-22 22:04:42 -0700

    created redux for addcartitems

--- client/src/components/collectionItem/CollectionItem.component.jsx
+++ client/src/components/collectionItem/CollectionItem.component.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React  from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
+
 import CollectionItemStyle from './styles';
 import CustomeButton from '../custom-button/customButton.component.jsx';
 
+
 const CollectionItem = ({
-  id, name, price, imageUrl,
-}) => (
-  <CollectionItemStyle>
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collectionFooter">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
+  item, addItem
+}) => {
+  const { id, name, price, imageUrl } = item;
+  return (
+    <CollectionItemStyle>
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collectionFooter">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
 
-    <CustomeButton inverted> Add to cart </CustomeButton>
-  </CollectionItemStyle>
-);
+      <CustomeButton onClick={() => addItem(item)}inverted> 
+        Add to cart 
+      </CustomeButton>
+    </CollectionItemStyle>
+  )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
